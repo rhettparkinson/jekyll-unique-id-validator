fbd89d815f62fb141fd6048ed0bd813ccea4f6b7
Rhett Parkinson <rhett@parkinson.au>
2023-09-19 16:04:46 +1000

    Update check_unique_ids.rb

--- check_unique_ids.rb
+++ check_unique_ids.rb
@@ -1,6 +1,6 @@
-# This Jekyll plugin ensures that all posts have a unique ID.
+# This Jekyll plugin ensures that all posts have a unique ID (uid).
 #
-# Every time the site is built, this plugin checks if each post has an 'id' key in its frontmatter.
+# Every time the site is built, this plugin checks if each post has an 'uid' key in its frontmatter.
 # It then checks if this ID is unique across all posts. If there's a duplicate or if the ID is missing,
 # the build process will halt with an error, ensuring the integrity of post IDs.
 
@@ -16,24 +16,24 @@ module Jekyll
 
     # This method is the main entry point for the plugin.
     def generate(site)
-      # Store all encountered IDs in a Set for fast lookup.
-      encountered_ids = Set.new
+      # Store all encountered UIDs in a Set for fast lookup.
+      encountered_uids = Set.new
 
       # Loop through all the posts.
       site.posts.docs.each do |post|
-        # Fetch the ID from the post's frontmatter.
-        id = post.data['id']        
+        # Fetch the UID from the post's frontmatter.
+        uid = post.data['uid']        
         # Fetch the title from the post's frontmatter.
         title = post.data['title']
-        # Check if the ID exists and if it has already been encountered.
-        if id.nil?
-          print_error("Post '#{title}' does not have an ID!")
-          raise "Post ID Error"
+        # Check if the UID exists and if it has already been encountered.
+        if uid.nil?
+          print_error("Post '#{title}' does not have an UID!")
+          raise "Post UID Error"
         elsif encountered_ids.include?(id)
-          print_error("Duplicate ID '#{id}' found in post '#{title}'!")
-          raise "Duplicate ID Error"
+          print_error("Duplicate UID '#{id}' found in post '#{title}'!")
+          raise "Duplicate UID Error"
         else
-          encountered_ids.add(id)
+          encountered_uids.add(uid)
         end
       end
     end
